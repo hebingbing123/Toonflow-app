5ed36d649cb541acb1db60c06195db48e4b770e2
clive <1119499507@qq.com>
2026-04-08 01:36:03 +0800

    refactor(frontend): split system probes account

--- frontend/lib/home_page.dart
+++ frontend/lib/home_page.dart
@@ -21,6 +21,7 @@ part 'home_page/skills_harness_controller.dart';
 part 'home_page/quality_reviews_controller.dart';
 part 'home_page/system_probes_controller.dart';
 part 'home_page/system_probes_models_catalog.dart';
+part 'home_page/system_probes_account.dart';
 part 'home_page/auth_session_controller.dart';
 part 'home_page/overview_controller.dart';
 part 'home_page/build_sections.dart';
