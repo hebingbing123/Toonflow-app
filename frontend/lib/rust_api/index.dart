c7f1248ce7ea294f784707e959f75360128c456d
clive <1119499507@qq.com>
2026-04-08 12:04:20 +0800

    refactor(frontend): split novels event models

--- frontend/lib/rust_api/index.dart
+++ frontend/lib/rust_api/index.dart
@@ -21,6 +21,7 @@ part 'harness_api.dart';
 part 'jobs_api.dart';
 part 'models_catalog.dart';
 part 'novels_models.dart';
+part 'novels_events_models.dart';
 part 'novels_crud.dart';
 part 'novels_legacy_api.dart';
 part 'novels_rest_api.dart';
