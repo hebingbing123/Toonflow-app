aa666bb9a81d8181b8e9e5ec1a993b9c8d72545c
clive <1119499507@qq.com>
2026-04-08 10:20:18 +0800

    refactor(frontend): split asset images probe

--- frontend/lib/home_page.dart
+++ frontend/lib/home_page.dart
@@ -12,6 +12,7 @@ import 'rust_api.dart';
 
 part 'home_page/project_editor.dart';
 part 'home_page/project_editor_legacy_probes.dart';
+part 'home_page/project_editor_assets_images_probe.dart';
 part 'home_page/project_editor_novels_rest_probe.dart';
 part 'home_page/project_editor_novels_legacy_probe.dart';
 part 'home_page/project_editor_novel_events_probe.dart';
